39dc20bfb7609777190314f66c7575020cba2027
Ore19095 <ore19095@uvg.edu.gt>
2024-01-02 22:26:53 -0600

    añadidas funciones uart solo para test

--- charging_system_v2.X/main.c
+++ charging_system_v2.X/main.c
@@ -11,18 +11,38 @@
 #include <util/delay.h>
 
 #include "controller_io_utils.h"
+#include "adc_utils.h"
 
 void main(void) {
-    // se configuran los puertos de entrada y salida
-    configurePorts();
 
+    configureTimer2();
+    configureADC();
+    conf_uart();
     while (1){
-        nimhLedOn();
         _delay_ms(1000);
-        nimhLedOff();
-        _delay_ms(1000);
+        send_data("Hola mundo\n", 11);
     }
     
 
     return;
+}
+
+void send_data(const char* data, uint8_t num){
+    for (int i=0; i<num && data[i]!=0; i++ ) {
+        while( !( UCSR0A&(1<<UDRE0))  );
+        UDR0 = data[i];
+        //uartBuffer[writePointer] = data[i];
+        //writePointer=(writePointer+1)%UART_BUFFER;
+    }
+    //UCSR0B |= 1<<UDRIE0; // HAbilitar interrupcion
+    return;
+}
+
+void conf_uart(){
+     UCSR0A |= 1<<U2X0; //UART en modo de alta velocidad
+    
+    UCSR0B |= 1<<RXEN0; // Habilitar receptor UART
+    UCSR0B |= 1<<TXEN0; // Habilitar transmisor UART
+   
+    UBRR0 = 103; //1M baud rate con Fosc = 8MHz
 }
