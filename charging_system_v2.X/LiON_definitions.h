282ea0834859b910866ddcd70947674a0bf610cf
Ore19095 <ore19095@uvg.edu.gt>
2024-01-04 21:30:37 -0600

    Constantes para carga de bateria LiON

---
+++ charging_system_v2.X/LiON_definitions.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   
+ * Author: 
+ * Comments: Contantes relacionadas con la bateria LiON, estos valores son los
+ * que deberá de mostrar el ADC.
+ * Revision history: 
+ */
+
+#ifndef LION_DEFINITIONS_H
+#define	LION_DEFINITIONS_H
+
+#define V_LION_FULL 860 // Voltaje de carga completa de la bateria LiON (4.2V)
+#define V_LION_EMPTY 614// Voltaje de descarga completa de la bateria LiON (3.0V)
+
+#define I_LION_CHARGE 1 // Corriente de carga de la bateria LION (A)
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
