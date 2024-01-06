6235c5474f48b52d8fd08f95b0bf8f49dfa23458
Ore19095 <ore19095@uvg.edu.gt>
2024-01-05 21:32:15 -0600

    cambio de umbrales y corriente de carga

--- Charging_system.X/LiON_definitions.h
+++ Charging_system.X/LiON_definitions.h
@@ -10,9 +10,9 @@
 #define	LION_DEFINITIONS_H
 
 #define V_LION_FULL 860 // Voltaje de carga completa de la bateria LiON (4.2V)
-#define V_LION_EMPTY 675// Voltaje de descarga completa de la bateria LiON (3.0V)
+#define V_LION_EMPTY 716// Voltaje de descarga completa de la bateria LiON (3.0V)
 
-#define I_LION_CHARGE 614//1A*3*1024/5 Corriente de carga de la bateria LION (A)
+#define I_LION_CHARGE 819//1A*3*1024/5 Corriente de carga de la bateria LION (A)
 #define I_LION_STOP_CHARGE 92//0.15A*3*1024/5 Corriente de carga de la bateria LION (A)
 
 #endif	/* XC_HEADER_TEMPLATE_H */
